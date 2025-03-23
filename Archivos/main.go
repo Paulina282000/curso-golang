5d056be5cdf11817f09d1581979a3be430d3bb0c
Paulina282000 <paulinatirante.28@gmail.com>
2025-03-22 21:59:19 -0300

    Primer arcivo go

---
+++ Archivos/main.go
@@ -0,0 +1,10 @@
+package main //nombre del packete, define que el achivo pertenece a main
+//en goi si o si tenes que pertenecer a un paquete
+
+import (
+	"fmt" //importa el paquete fmt, que es el paquete de entrada y salida de datos
+) //para usar println
+
+func main() { //la funcion se encarga de ejecutar el programa
+	fmt.Println("Hello, World!")
+}
